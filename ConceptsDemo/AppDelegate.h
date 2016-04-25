03e8839e300edead90219ccf53cfe8f1fd66cf13
bhaumikdesai91 <bhaumikdesai91@icloud.com>
2016-04-25 15:01:48 +0530

    Initial Commit

---
+++ ConceptsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConceptsDemo
+//
+//  Created by Bhaumik P. Desai on 25/04/16.
+//  Copyright © 2016 Bhaumik P. Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConceptsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConceptsDemo
+//
+//  Created by Bhaumik P. Desai on 25/04/16.
+//  Copyright © 2016 Bhaumik P. Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
